e0af3b19e9c9d9b2d331677a5e7b5e2f6ff18dc4
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-11-26 14:16:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='Chalk',
+    name='ChalkCli',
     version='0.1.0',
     author='Ishan Oshada',
     author_email='Ishan.kodithuwakku.official@gmail.com',
     description='A Python package for terminal text styling and coloring.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/ishanoshada/Chalk',  # Replace with your GitHub repository link
+    url='https://github.com/ishanoshada/ChalkCli',  
     packages=find_packages(),
      classifiers=[
         'Programming Language :: Python :: 3',
